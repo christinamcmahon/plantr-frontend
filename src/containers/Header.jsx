d2d83a24e13f0bc220e564fa072ee86a45863604
christinamcmahon <christina.mcmahon1@gmail.com>
2020-03-23 09:24:34 -0700

    add color to header

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
 import EcoIcon from '@material-ui/icons/Eco';
 
+const styles = {
+    header: {
+        backgroundColor: 'rgb(88, 141, 114)'
+    }
+}
+
 const Header = () => {
     return (
-        <AppBar position="static">
+        <AppBar position="static" style={styles.header}>
             <Toolbar>
                 <EcoIcon />
                 <Typography>plantr</Typography>
