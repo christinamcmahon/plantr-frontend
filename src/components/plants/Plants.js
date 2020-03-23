f3413ce6565c04ec3bc65e56f32b10dea8b863a7
christinamcmahon <christina.mcmahon1@gmail.com>
2020-03-23 11:30:55 -0700

    add grid formatting to plants

--- src/components/plants/Plants.js
+++ src/components/plants/Plants.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Plant from "./Plant";
+import { Grid } from "@material-ui/core"
+import { Redirect } from 'react-router'
 
 class Plants extends Component {
     render() {
@@ -10,19 +12,26 @@ class Plants extends Component {
                 console.log('INSIDE PLANTS plants', plants)
                 console.log("INSIDE PLANTS plant", plantInstance)
                 return (
-                    <Plant
-                        key={plantInstance.id}
-                        plantProps={plantInstance}
-                        // plants={plants}
-                        deletePlant={this.props.deletePlant}
-                        updatePlant={this.props.updatePlant}
-                    />
+                    <Grid item xs={12} sm={4}>
+                        <Plant
+                            key={plantInstance.id}
+                            plantProps={plantInstance}
+                            deletePlant={this.props.deletePlant}
+                            updatePlant={this.props.updatePlant}
+                        />
+                    </Grid>
                 );
             });
         } else {
             return null;
         }
-        return <ul style={{ listStyleType: "none" }}>{plantsList}</ul>;
+        // return this.props.loggedIn ? (
+        return (
+            <Grid container spacing={2} style={{ marginTop: '10vh' }}>
+                {plantsList}
+            </Grid>
+        )
+        // ) : <Redirect to="/login" />;
     }
 }
 
