155a947bb0ab7dac6bce384ea01154ae7402c43a
christinamcmahon <christina.mcmahon1@gmail.com>
2020-03-23 11:30:33 -0700

    style add plant form

--- src/components/plants/PlantInput.js
+++ src/components/plants/PlantInput.js
@@ -2,13 +2,22 @@ import React, { Component } from "react";
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import { addPlant } from '../../actions/plant'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slider from '@material-ui/core/Slider';
+import Typography from '@material-ui/core/Typography';
 
 class PlantInput extends Component {
     state = {
         name: "",
         notes: "",
         water_frequency: "",
-        image_url: "https://png.pngtree.com/element_our/png_detail/20181229/sprout-plant-graphic-icon-design-template-png_301529.jpg"
+        image_url: "https://png.pngtree.com/element_our/png_detail/20181229/sprout-plant-graphic-icon-design-template-png_301529.jpg",
+        add_plant_mode: false
     };
 
     handleOnSubmit = e => {
@@ -24,8 +33,14 @@ class PlantInput extends Component {
             name: "",
             notes: "",
             water_frequency: "",
-            image_url: "https://png.pngtree.com/element_our/png_detail/20181229/sprout-plant-graphic-icon-design-template-png_301529.jpg"
+            image_url: "https://png.pngtree.com/element_our/png_detail/20181229/sprout-plant-graphic-icon-design-template-png_301529.jpg",
+            add_plant_mode: false
         });
+    };
+
+    handleOpenCloseAddPlant = () => {
+        const toggledValue = !this.state.add_plant_mode
+        this.setState({ add_plant_mode: toggledValue });
     };
 
     handleOnChangeName = e => {
@@ -37,7 +52,8 @@ class PlantInput extends Component {
     };
 
     handleOnChangeWaterFrequency = e => {
-        this.setState({ water_frequency: e.target.value });
+        console.log("INSIDE CHANGE WATER FREQ", e)
+        this.setState({ water_frequency: e.target.innerText });
     };
 
     handleOnChangeImageUrl = e => {
@@ -46,40 +62,35 @@ class PlantInput extends Component {
 
     render() {
         console.log("PLANT INPUT PROPS: ", this.props)
+        console.log("PLANT INPUT STATE: ", this.state)
         return (
             <div>
-                <form onSubmit={this.handleOnSubmit}>
-                    <label>Name: </label>
-                    <input
-                        type="text"
-                        value={this.state.name}
-                        onChange={this.handleOnChangeName}
-                    />
-                    <br />
-                    <label>Notes: </label>
-                    <input
-                        type="text"
-                        value={this.state.notes}
-                        onChange={this.handleOnChangeNotes}
-                    />
-                    <br />
-                    <label>Water Frequency: </label>
-                    <input
-                        type="number"
-                        value={this.state.water_frequency}
-                        onChange={this.handleOnChangeWaterFrequency}
-                    />
-                    <br />
-                    <label>Image: </label>
-                    <input
-                        type="text"
-                        alt="not available"
-                        value={this.state.image_url}
-                        onChange={this.handleOnChangeImageUrl}
-                    />
-                    <br />
-                    <input type="submit" value="Submit" />
-                </form>
+                <Button variant="outlined" color="primary" onClick={this.handleOpenCloseAddPlant}>
+                    Add Plant
+                </Button>
+                <Dialog onSubmit={this.handleOnSubmit} open={this.state.add_plant_mode} onClose={this.handleOpenCloseAddPlant} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Add Plant</DialogTitle>
+                    <DialogContent>
+                        <TextField autoFocus label="Name" fullWidth onChange={this.handleOnChangeName} />
+                        <TextField label="Notes" fullWidth onChange={this.handleOnChangeNotes} multiline rows={4} />
+                        <Typography id="input-slider">
+                            Water Frequency
+                        </Typography>
+                        <Slider onChange={this.handleOnChangeWaterFrequency} aria-labelledby="input-slider" defaultValue={7} valueLabelDisplay="on" max={30} />
+                        {/* <input type="file" display="none" id="upload-photo" style={{ display: "none" }} />
+                        <label htmlFor="upload-photo">
+                            <Button variant="outlined" color="primary" component="span">
+                                Upload Photo
+                            </Button>
+                        </label> */}
+                        <TextField label="Image URL" fullWidth onChange={this.handleOnChangeImageUrl} />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button type="submit" fullWidth variant="contained" color="primary" onClick={this.handleOnSubmit}>
+                            Submit
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
