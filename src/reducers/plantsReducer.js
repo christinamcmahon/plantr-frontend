ec5282ddef1b9ab3b9178d131be303dbda9ee7b1
christinamcmahon <christina.mcmahon1@gmail.com>
2020-03-23 19:25:51 -0700

    add fetch plants

--- src/reducers/plantsReducer.js
+++ src/reducers/plantsReducer.js
@@ -9,13 +9,20 @@ export default function plantsReducer(state = defaultState, action) {
     console.log('INSIDE PLANTS REDUCER action', action)
     console.log('INSIDE PLANTS REDUCER state', state)
     switch (action.type) {
+        case "FETCH_PLANTS":
+            console.log("INSIDE plantsReducer FETCH", action.payload.plant_info.plants)
+            return {
+                ...state,
+                plants: action.payload.plant_info.plants.map(plant => {
+                    return plant
+                })
+            }
         case "ADD_PLANT":
             return {
                 ...state,
                 plants: [...state.plants, action.payload]
             }
         case "DELETE_PLANT":
-            // idx = state.plants.findIndex(plant => plant.id === action.id)
             let newPlants = state.plants.filter((plant) => plant.id !== action.payload.id)
             console.log("INSIDE DELETE_PLANT", action, newPlants)
             return {
